13a7a68a5d7f5cf00cfc50a13ad4c39f857285a0
Sworup Shrestha <98733444+SSworup77@users.noreply.github.com>
2025-03-10 13:02:16 +0545

    Update blog_db.sql

--- _db/blog_db.sql
+++ _db/blog_db.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 21, 2023 at 09:41 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
